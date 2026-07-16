e76484e4adb5f98099022a67f360e8a411ae69a4
Tetiana <tetiana@MacBook-Air-Tetiana.local>
2026-07-16 12:41:19 +0300

    refactor(auth): bind TokenPayload interface to CurrentUser execution context

--- backend/src/auth/current-user.decorator.ts
+++ backend/src/auth/current-user.decorator.ts
@@ -1,7 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+import { TokenPayload } from './token-payload.interface';
 
 const getCurrentUserByContext = (context: ExecutionContext) => {
-  return context.switchToHttp().getRequest().user;
+  return context.switchToHttp().getRequest<Request & { user?: TokenPayload }>()
+    .user;
 };
 
 export const CurrentUser = createParamDecorator(
