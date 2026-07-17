9022b49882b1e11c97b1c04a0e87d043cf6e1732
Tetiana <tetiana@MacBook-Air-Tetiana.local>
2026-07-17 13:13:50 +0300

    refactor(auth): implement type-safe smart CurrentUser decorator

--- backend/src/auth/current-user.decorator.ts
+++ backend/src/auth/current-user.decorator.ts
@@ -2,12 +2,19 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 import { TokenPayload } from './token-payload.interface';
 
-const getCurrentUserByContext = (context: ExecutionContext) => {
-  return context.switchToHttp().getRequest<Request & { user?: TokenPayload }>()
-    .user;
+const getCurrentUserByContext = (
+  data: keyof TokenPayload | undefined,
+  context: ExecutionContext,
+) => {
+  const request = context
+    .switchToHttp()
+    .getRequest<Request & { user?: TokenPayload }>();
+  const user = request.user as TokenPayload;
+  if (!user) return null;
+  return data ? user[data] : user;
 };
 
 export const CurrentUser = createParamDecorator(
-  (_data: unknown, context: ExecutionContext) =>
-    getCurrentUserByContext(context),
+  (data: keyof TokenPayload | undefined, context: ExecutionContext) =>
+    getCurrentUserByContext(data, context),
 );
