02ffcc64e9aa8d52c68cc22478bf0b1fc092e582
Tetiana <tetiana@MacBook-Air-Tetiana.local>
2026-07-26 00:02:43 +0300

    refactor(projects): replace static view with dynamic ProjectDetails component on project page

--- frontend/app/(dashboard)/project/[projectSlug]/page.tsx
+++ frontend/app/(dashboard)/project/[projectSlug]/page.tsx
@@ -1,12 +1,9 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { routes } from "@/app/common/constants/routes";
 import { assertNoErrors } from "@/app/common/util/error-redirect";
-import { formatDate } from "@/app/common/util/format-date";
 import getProject from "./actions/get-project";
 import { redirect } from "next/navigation";
 import { toSlug } from "@/app/common/util/to-slug";
-import { type Project } from "./interfaces/project.interface";
+import { ProjectDetails } from "./components/project-details";
 
 interface ProjectProps {
   params: Promise<{ projectSlug: string }>;
@@ -28,22 +25,5 @@ export default async function Project({ params }: ProjectProps) {
     `${project.id}-${toSlug(project.name)}`,
   );
 
-  return (
-    <>
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-        {project.name}
-      </h2>
-      <h4 className="scroll-m-20 font-semibold tracking-tight text-muted-foreground">
-        {formatDate(project.createdAt, true)}
-      </h4>
-      <h4 className="scroll-m-20 font-semibold tracking-tight text-muted-foreground">
-        {formatDate(project.updatedAt, true)}
-      </h4>
-      <p>{project.description}</p>
-      <p>Some details will be soon</p>
-      <Button variant="outline" className="w-full" asChild>
-        <Link href={taskLink}>Tasks</Link>
-      </Button>
-    </>
-  );
+  return <ProjectDetails project={project} taskLink={taskLink} />;
 }
