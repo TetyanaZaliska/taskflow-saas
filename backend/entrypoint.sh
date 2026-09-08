a75744786402508fdd4c652c7b2e1ea900bf624f
Tetiana <tetiana@MacBook-Air-Tetiana.local>
2026-09-08 13:22:58 +0300

    chore: change docker for prisma8 rules

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,41 @@
 #!/bin/sh
 
-# 1. Wait for 3 seconds, to PostgreSQL get up in memory
-sleep 3
 
-echo "🚀 Running automatic database migrations..."
-npx prisma db push
 
-echo "🔥 Starting NestJS Application..."
-node dist/main
+echo "⏳ Waiting for PostgreSQL to be ready..."
+
+if [ -z "$POSTGRES_HOST" ] || [ -z "$POSTGRES_PORT" ]; then
+  echo "❌ ERROR: POSTGRES_HOST or POSTGRES_PORT environment variables are not set!"
+  exit 1
+fi
+
+set +e
+
+while true; do
+  # Пробуємо підключитися, перенаправляючи помилки в порожнечу
+  nc -z -w3 "$POSTGRES_HOST" "$POSTGRES_PORT" > /dev/null 2>&1
+  
+  
+  if [ $? -eq 0 ]; then
+    break
+  fi
+
+  echo "⚠️ Database server ($POSTGRES_HOST:$POSTGRES_PORT) is unavailable - sleeping 1s"
+  sleep 1
+done
+
+set -e
+
+echo "✅ Database is up and responding!"
+
+cp prisma.config.mts prisma.config.ts
+
+echo "🚀 Applying database migrations via Prisma 8 Graph..."
+# --yes autoconfirm migrations in Docker
+npx prisma db update --yes
+
+rm prisma.config.ts
+
+echo "🔥 Starting NestJS Application with Graceful Shutdown..."
+# exec makes Node.js process PID 1 for proper SIGTERM
+exec node dist/main
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,9 +8,25 @@ import { TeamMembersModule } from './team-members/team-members.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ProjectModule } from './project/project.module';
 import { TasksModule } from './tasks/tasks.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const redisPort = configService.get<string>('REDIS_PORT') || '6379';
+        const redisUrl = `redis://localhost:${redisPort}`;
+
+        return {
+          connection: {
+            url: redisUrl,
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
     LoggerModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
@@ -32,7 +48,6 @@ import { TasksModule } from './tasks/tasks.module';
       },
       inject: [ConfigService],
     }),
-    ConfigModule.forRoot(),
     UsersModule,
     AuthModule,
     TeamsModule,
